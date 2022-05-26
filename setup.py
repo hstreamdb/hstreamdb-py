066e46f0339b82b7b3c151b31f27c3aa44106b86
mu <59917266+4eUeP@users.noreply.github.com>
2022-05-26 13:55:34 +0800

    basic client

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hstreamdb",
+    version="0.0.1",
+    author="lambda",
+    author_email="lambda@emqx.io",
+    description="Python client for HStreamDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hstreamdb/hstreamdb-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/hstreamdb/hstreamdb-py/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "grpcio",
+    ],
+    python_requires=">=3.7",
+)
