bb2f40ad5c3571d46d78547ab73ed5401a364951
mu <59917266+4eUeP@users.noreply.github.com>
2022-06-14 16:36:19 +0800

    use "requirements.txt" instead

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    install_requires = fh.read().splitlines()
+
 setuptools.setup(
     name="hstreamdb",
     version="0.0.1",
     author="lambda",
     author_email="lambda@emqx.io",
     description="Python client for HStreamDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/hstreamdb/hstreamdb-py",
     project_urls={
         "Bug Tracker": "https://github.com/hstreamdb/hstreamdb-py/issues",
@@ -16,10 +24,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(exclude=["tests"]),
-    install_requires=[
-        "grpcio",
-        "protobuf",
-        "hstreamdb-api>=0.0.2",
-    ],
+    install_requires=install_requires,
     python_requires=">=3.7",
 )
